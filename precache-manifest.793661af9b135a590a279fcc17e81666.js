b654a9895b603a627af2e43ac26db6bbb64244c0
sylvestrevgen <sylvestrevgen@gmail.com>
2019-09-12 01:02:47 +0300

    Updates

---
+++ precache-manifest.793661af9b135a590a279fcc17e81666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7be9587fe055bc5ccdcdf907be12f1c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "240c05043c16d233e189",
+    "url": "/goit-react-hw-01-components/static/css/main.9e5d7732.chunk.css"
+  },
+  {
+    "revision": "eef84db5965f86bdfad5",
+    "url": "/goit-react-hw-01-components/static/js/2.ac59fa37.chunk.js"
+  },
+  {
+    "revision": "240c05043c16d233e189",
+    "url": "/goit-react-hw-01-components/static/js/main.e42fccf5.chunk.js"
+  },
+  {
+    "revision": "620ceb3fc25e2e9c8f81",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.a6ab4004.js"
+  }
+]);
